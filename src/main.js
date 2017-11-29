72b5d5312b5fb4f9f2dbe05bfa53e5165389795a
mjyplusone <1546331221@qq.com>
2017-11-29 10:58:16 +0800

    修改目录结构，配置eslint、alias等

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+// import router from './router'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   render: h => h(App)
-})
+});
